a1f4c8dd7a11b926c7e422d97b6929d93e9c61d6
Jeongmin <balljm@naver.com>
2024-04-02 10:59:58 +0900

    modify linkedin link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,7 +31,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/jeongmin-seo-6239b91b7/"
+                href="https://www.linkedin.com/in/jeongminseo/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -57,7 +57,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/jeongmin-seo-6239b91b7/"
+                  href="https://www.linkedin.com/in/jeongminseo/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
