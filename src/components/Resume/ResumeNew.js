60956524d046654579099ba10fddd4973253ea60
Jeongmin <balljm@naver.com>
2024-05-22 10:29:54 +0900

    modify resume for aws application

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_JeongminSeo_English.pdf";
+import pdf from "../../Assets/../Assets/AWS_ASA_Resume_JeongminSeo.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
